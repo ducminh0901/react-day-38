1e49867b45972cb6934386f73177caf4bada1eae
DucMinh <minhndf8@fullstack.edu.vn>
2025-11-18 14:27:45 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    base: "/f8-day-38/",
+    base: "/f8-react-day-38/",
 });
